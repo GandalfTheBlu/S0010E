cc89429cb67537513c9e925bf4903fac3af63684
GandalfTheBlu <gandalflindkvist@gmail.com>
2022-11-07 15:28:18 +0100

    redid lab2 task 3 and 4

--- gansel_Lab1_Task4/Intermediate/Build/Win64/gansel_Lab1_Task4Editor/Development/Engine/SharedPCH.Engine.ShadowErrors.cpp
+++ gansel_Lab1_Task4/Intermediate/Build/Win64/gansel_Lab1_Task4Editor/Development/Engine/SharedPCH.Engine.ShadowErrors.cpp
@@ -1,2 +1,2 @@
-// Compiler: 14.29.30136
-#include "C:/Users/Gandalf/Documents/LTU/PROGRAMVARUTEKNIK/S0010E/gansel_Lab1_Task4/Intermediate/Build/Win64/gansel_Lab1_Task4Editor/Development/Engine/SharedPCH.Engine.ShadowErrors.h"
+// Compiler: 14.33.31630
+#include "C:/Users/Gandalf/git/S0010E/gansel_Lab1_Task4/Intermediate/Build/Win64/gansel_Lab1_Task4Editor/Development/Engine/SharedPCH.Engine.ShadowErrors.h"
--- gansel_Lab2_Task3to5/Intermediate/Build/Win64/gansel_Lab2_Task3to5Editor/Development/Engine/SharedPCH.Engine.ShadowErrors.cpp
+++
@@ -1,2 +0,0 @@
-// Compiler: 14.33.31630
-#include "C:/Users/Gandalf/git/S0010E/gansel_Lab2_Task3to5/Intermediate/Build/Win64/gansel_Lab2_Task3to5Editor/Development/Engine/SharedPCH.Engine.ShadowErrors.h"
